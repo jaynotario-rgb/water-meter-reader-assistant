0a5674c7de14cca1434ec30cdcd5506212e109a8
jaynotario-rgb <jaynotario@gmail.com>
2026-08-15 18:42:57 +0800

    load audit state styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { registerSW } from 'virtual:pwa-register';
 import { App } from './App';
 import './styles.css';
 import './enhancements.css';
+import './audit.css';
 
 registerSW({ immediate: true });
 
