6c2fb516df493a6a0649e1abfd74f120a6b28fec
jaynotario-rgb <jaynotario@gmail.com>
2026-08-16 10:13:09 +0800

    wire pilot settings controls

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import { App } from './App';
+import { PilotControls } from './PilotControls';
 import './styles.css';
 import './enhancements.css';
 import './audit.css';
+import './pilot.css';
 
 registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <PilotControls />
   </React.StrictMode>,
 );
